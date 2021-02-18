c5a7fbf52fc6ab57156dbcbef3d9f9bc9133e25d
meXan1k778 <dg9042247@gmail.com>
2021-02-18 17:46:41 +0200

    feat: added banner component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from '../Header';
+import MainBanner from '../MainBanner';
+
 
 function App() {
   return (
     <div>
      <Header />
+     <MainBanner />
     </div>
   );
 }
---
+++ src/styles/mixins.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+// export const CtaButton = styled.h1<ICtaButton>`
+export const LayoutWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+  max-width: 1260px;
+  margin: 0 auto;
+  padding: 0 20px;
+`;
+
+export const SimpleButton = styled.button`
+  color: #fff;
+  background-color: transparent;
+  border: none;
+  font-size: 16px;
+  line-height: 19px;
+`;
+
+export const CtaButton = styled.button`
+  padding: 11px 36px;
+  background: #B2212B;
+  color: #fff;
+  border: none;
+  font-size: 16px;
+  line-height: 19px;
+  border-radius: 8px;
+  
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const Paragraph = styled.p`
+  font-size: 14px;
+  line-height: 17px;
+  margin: 16px 0;
+`;
